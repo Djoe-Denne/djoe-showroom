304ad1329635431b207f7568e177b1b6b8b4c86d
Djoe-Denne <djoe.denne@blablacar.com>
2023-03-11 16:20:31 +0100

    initial commit

---
+++ dummy-service/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.0.4"
+	id("io.spring.dependency-management") version "1.1.0"
+	id("org.graalvm.buildtools.native") version "0.9.20"
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "dummy-service"
+
+include("dummy-service")
